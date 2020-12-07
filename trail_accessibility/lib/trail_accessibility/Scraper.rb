431b66458d6510174a508e5977cdfdd6cc87624a
Allison _ <allimacq@gmail.com>
2020-12-07 23:48:01 +0000

    renamed file to Trail and added a few methods to it. Also added methods to Scraper class

--- trail_accessibility/lib/trail_accessibility/Scraper.rb
+++ trail_accessibility/lib/trail_accessibility/Scraper.rb
@@ -1,35 +1,55 @@
-require 'nokogiri'
-require 'open-uri'
-
-require_relative './Park.rb'
-
 class Scraper
-  site = 'https://www.traillink.com/activity/wheelchair-accessible-trails/'
-  page = Nokogiri::HTML(open(site))
   
-  #this method scrapes the website to create and return a hash for each state containing the state name and its associated url for the site. 
-  def self.get_states(site)
-    results = page.css('div.row.small-up-2.medium-up-3.large-up-4.block-grid div.column')
-
+  #this method gets all the states and a link to their page of accessibil trails. the return is an array of hashes.
+  def get_states
+    site = 'https://www.traillink.com/activity/wheelchair-accessible-trails/'
+    page = Nokogiri::HTML(open(site))
+    results = page.css("div.row.small-up-2.medium-up-3.large-up-4.block-grid div.column")
+    
+    #initializing an array to hold the hashes of states
     array = []
-
+    
     results.each do |state|
-      # storing the state and it's url in an array as an individual hash
       array << {
-        # state name is the key
-        state.css('a').text.strip => state.css('a').attribute('href').value
-        # state url is the value
+        state: state.css("a").text.strip
+        link: state.css("a").attribute("href").value
       }
     end
-
-    # only want the states and DC links so using the take method to achieve this.
-    state_array = array.take(51)
+    
+    #the website has other categories beyone states so using the take method to get just the states and dc
+    states_array = array.take(51)
   end
   
-  ##maybe I should make this all one method?
-  
-  #the list park method uses the array of hashes above to scrape for a list of accessible trails in that state.
-  def self.list_parks(state_hash)
+  #this method gets the state trail page ready to scrape for the necessary info
+  def get_requested(state)
+    #using the find method to get the link to the user's requested state
+    state_to_find = self.get_states.find {|h| h[:state] == "#{state}"}[:link]
+    
+    #this is the link that will be used with Nokogiri
+    state_link = 'https://traillink/com' + state_to_find
+    
+    #opening the requested state's page
+    state_page = Nokogiri::HTML(open(state_link))
+    
+    #getting the table of trails for requested state
+    state_results = state_page.css('table.search-result-table tbody tr.search-result-card.hide-for-small-only')
   end
+  
+  #will use this method to create Trails 
+  def make_state_trails(state)
+    self.get_requested(state).collect do |state_trails|
+      trail = Trail.new
+      trail.link = state_trails.css("td.info div a").attribute("href").value
+      trail.name = state_trails.css("td.info div a h3").text.strip
+      trail.info = state_trails.css("td.info div")[1].text
+      trail.state = state_trails.css("td.states").text.strip
+      trail.length = state_trails.css("td.length").text.strip
+      trail.surface = state_trails.css("td.surface").text.strip
+      trail.rating = state_trails.css("td.rating div a").text.strip
+      trail.rating_link = state_trails.css("td.rating div a").attribute("href").value
+    end
+  end
+  
+  
   
 end
---
+++ trail_accessibility/lib/trail_accessibility/Trail.rb
@@ -0,0 +1,19 @@
+class Trail
+  
+  attr_accessor :state, :name, :link, :info, :description, :length, :surface, :rating, :rating_link
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def .all
+    @@all
+  end
+  
+  def clear_all
+    @@all.clear
+  end
+  
+end
